1c7b6a87a63284c75e0015b67ac320b6bf70a13d
davinci150 <davinci150cc@gmail.com>
2022-12-07 17:41:06 +0300

    feat: expanded section

---
+++ lib/widget/expandable_section.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+class ExpandedSection extends StatefulWidget {
+  const ExpandedSection({Key? key, this.expand = true, required this.child})
+      : super(key: key);
+
+  final Widget child;
+  final bool expand;
+
+  @override
+  _ExpandedSectionState createState() => _ExpandedSectionState();
+}
+
+class _ExpandedSectionState extends State<ExpandedSection>
+    with SingleTickerProviderStateMixin {
+  late AnimationController expandController;
+  late Animation<double> animation;
+
+  @override
+  void initState() {
+    super.initState();
+    prepareAnimations();
+    _runExpandChecks();
+  }
+
+  void prepareAnimations() {
+    expandController = AnimationController(
+        vsync: this, duration: const Duration(milliseconds: 500));
+    animation = CurvedAnimation(
+      parent: expandController,
+      curve: Curves.fastOutSlowIn,
+    );
+  }
+
+  void _runExpandChecks() {
+    if (widget.expand) {
+      expandController.forward(from: 1);
+    } else {
+      expandController.reverse(from: 0);
+    }
+  }
+
+  void _runExpandCheck() {
+    if (widget.expand) {
+      expandController.forward();
+    } else {
+      expandController.reverse();
+    }
+  }
+
+  @override
+  void didUpdateWidget(ExpandedSection oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    _runExpandCheck();
+  }
+
+  @override
+  void dispose() {
+    expandController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SizeTransition(
+        axisAlignment: 1.0, sizeFactor: animation, child: widget.child);
+  }
+}
--- lib/widget/task_item_widget.dart
+++ lib/widget/task_item_widget.dart
@@ -2,11 +2,13 @@ import 'dart:async';
 import 'dart:developer';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:intl/intl.dart';
 import 'package:todo_task/model/group_model.dart';
 import '../model/task_model.dart';
+import 'expandable_section.dart';
 
-class TextItemWidget extends StatelessWidget {
+class TextItemWidget extends StatefulWidget {
   const TextItemWidget(
       {Key? key,
       required this.model,
@@ -21,51 +23,60 @@ class TextItemWidget extends StatelessWidget {
   final VoidCallback onTapDelete;
 
   @override
+  State<TextItemWidget> createState() => _TextItemWidgetState();
+}
+
+class _TextItemWidgetState extends State<TextItemWidget> {
+  bool isExpand = true;
+  @override
   Widget build(BuildContext context) {
     return Container(
-      padding: model.tasks!.isEmpty ? null : const EdgeInsets.all(8),
-      margin: model.tasks!.isEmpty
+      padding: widget.model.tasks!.isEmpty ? null : const EdgeInsets.all(8),
+      margin: widget.model.tasks!.isEmpty
           ? const EdgeInsets.fromLTRB(20, 4, 20, 4)
           : const EdgeInsets.fromLTRB(12, 4, 12, 4),
-      decoration: model.tasks!.isNotEmpty
+      decoration: widget.model.tasks!.isNotEmpty
           ? BoxDecoration(
               color: Colors.grey.withOpacity(0.1),
               borderRadius: BorderRadius.circular(10))
           : null,
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         mainAxisSize: MainAxisSize.min,
         children: [
           Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Padding(
                 padding: const EdgeInsets.only(right: 6),
                 child: ReorderableDragStartListener(
-                  index: index,
+                  index: widget.index,
                   child: Icon(
                     Icons.drag_handle,
-                    color: Theme.of(context).iconTheme.color?.withOpacity(0.3),
+                    color:
+                        Theme.of(context).iconTheme.color?.withOpacity(0.3),
                     size: 16,
                   ),
                 ),
               ),
               CheckboxCustom(
                 onChanged: (value) {
-                  final newModel =
-                      model.copyWith(isDone: value, isVisible: value != null);
+                  final newModel = widget.model
+                      .copyWith(isDone: value, isVisible: value != null);
 
-                  onChanged(newModel);
+                  widget.onChanged(newModel);
                 },
-                disabled: model.isVisible == false,
-                value: model.isDone,
+                disabled: widget.model.isVisible == false,
+                value: widget.model.isDone,
               ),
               const SizedBox(width: 6),
               Expanded(
                 child: TextFormField(
                   maxLines: null,
-                  initialValue: model.text,
+                  initialValue: widget.model.text,
                   onChanged: (text) {
-                    final newModel = model.copyWith(text: text);
-                    onChanged(newModel);
+                    final newModel = widget.model.copyWith(text: text);
+                    widget.onChanged(newModel);
                   },
                   decoration: InputDecoration(
                     hintText: 'Enter the text',
@@ -76,40 +87,71 @@ class TextItemWidget extends StatelessWidget {
                   ),
                 ),
               ),
+              if (widget.model.tasks!.isNotEmpty)
+                Padding(
+                  padding: const EdgeInsets.only(right: 6.0),
+                  child: InkWell(
+                    onTap: copyToCliboard,
+                    child: const Icon(
+                      Icons.copy,
+                      color: Colors.grey,
+                      size: 16,
+                    ),
+                  ),
+                ),
+              if (widget.model.tasks!.isNotEmpty)
+                Padding(
+                  padding: const EdgeInsets.only(right: 6.0),
+                  child: InkWell(
+                    onTap: () {
+                      isExpand = !isExpand;
+                      setState(() {});
+                    },
+                    child: Icon(
+                      isExpand
+                          ? Icons.expand_less_outlined
+                          : Icons.keyboard_arrow_down_outlined,
+                      color: Colors.grey,
+                      size: 16,
+                    ),
+                  ),
+                ),
+              const SizedBox(width: 6),
               InkWell(
                   onTap: () {
-                    if (model.tasks!.isEmpty) {
-                      final tasks = List.of(model.tasks!)
+                    if (widget.model.tasks!.isEmpty) {
+                      final tasks = List.of(widget.model.tasks!)
                         ..add(TaskModel(
                             id: 0,
-                            text: model.text,
-                            isDone: model.isDone,
-                            createdOn: model.createdOn,
-                            isVisible: model.isVisible));
+                            text: widget.model.text,
+                            isDone: widget.model.isDone,
+                            createdOn: widget.model.createdOn,
+                            isVisible: widget.model.isVisible));
 
-                      final newModel =
-                          model.copyWith(tasks: tasks, text: '', isDone: false);
-                      onChanged(newModel);
+                      final newModel = widget.model
+                          .copyWith(tasks: tasks, text: '', isDone: false);
+                      widget.onChanged(newModel);
                     }
                     //
                     else {
-                      final tasks = List.of(model.tasks!)
+                      final tasks = List.of(widget.model.tasks!)
                         ..add(TaskModel(
-                            id: model.tasks!.length,
+                            id: widget.model.tasks!.length,
                             text: '',
                             isDone: false,
                             createdOn: DateTime.now(),
                             isVisible: true));
-                      final newModel = model.copyWith(tasks: tasks);
-                      onChanged(newModel);
+                      final newModel = widget.model.copyWith(tasks: tasks);
+                      widget.onChanged(newModel);
                     }
                   },
                   child: Icon(
-                    model.tasks!.isEmpty ? Icons.copy_all : Icons.add,
+                    widget.model.tasks!.isEmpty ? Icons.copy_all : Icons.add,
                     size: 16,
                   )),
+              const SizedBox(width: 6),
               InkWell(
-                onTap: onTapDelete,
+                onTap: widget.onTapDelete,
                 child: Icon(
                   Icons.close,
                   color: Colors.red.withOpacity(0.4),
@@ -118,72 +160,137 @@ class TextItemWidget extends StatelessWidget {
               ),
             ],
           ),
-          Column(
-            children: model.tasks!.map((item) {
-              final i = model.tasks!.indexOf(item);
-              return Container(
-                padding: const EdgeInsets.only(left: 42, top: 6),
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Row(
-                      children: [
-                        CheckboxCustom(
-                          onChanged: (value) {
-                            final newTask = item.copyWith(
-                                isDone: value, isVisible: value != null);
-                            final tasks = List.of(model.tasks!)..[i] = newTask;
-                            final newModel = model.copyWith(tasks: tasks);
-                            onChanged(newModel);
+          ExpandedSection(
+            expand: isExpand,
+            child: Column(
+              children: widget.model.tasks!.map((item) {
+                final i = widget.model.tasks!.indexOf(item);
+                return Container(
+                  padding: const EdgeInsets.only(left: 42, top: 6),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        children: [
+                          CheckboxCustom(
+                            onChanged: (value) {
+                              final newTask = item.copyWith(
+                                  isDone: value, isVisible: value != null);
+                              final tasks = List.of(widget.model.tasks!)
+                                ..[i] = newTask;
+                              final newModel =
+                                  widget.model.copyWith(tasks: tasks);
+                              widget.onChanged(newModel);
+                            },
+                            disabled: item.isVisible == false,
+                            value: item.isDone,
+                          ),
+                          const SizedBox(width: 6),
+                        ],
+                      ),
+                      Expanded(
+                        child: TextFormField(
+                          key: ValueKey(item.id),
+                          maxLines: null,
+                          initialValue: item.text,
+                          onChanged: (text) {
+                            final newTask = item.copyWith(text: text);
+                            final tasks = List.of(widget.model.tasks!)
+                              ..[i] = newTask;
+                            final newModel =
+                                widget.model.copyWith(tasks: tasks);
+                            widget.onChanged(newModel);
                           },
-                          disabled: item.isVisible == false,
-                          value: item.isDone,
-                        ),
-                        const SizedBox(width: 6),
-                      ],
-                    ),
-                    Expanded(
-                      child: TextFormField(
-                        key: ValueKey(item.id),
-                        maxLines: null,
-                        initialValue: item.text,
-                        onChanged: (text) {
-                          final newTask = item.copyWith(text: text);
-                          final tasks = List.of(model.tasks!)..[i] = newTask;
-                          final newModel = model.copyWith(tasks: tasks);
-                          onChanged(newModel);
-                        },
-                        decoration: InputDecoration(
-                          hintText: 'Enter the text',
-                          hintStyle:
-                              TextStyle(color: Colors.grey.withOpacity(0.5)),
-                          contentPadding: const EdgeInsets.fromLTRB(0, 2, 0, 0),
-                          isDense: true,
-                          border: InputBorder.none,
+                          decoration: InputDecoration(
+                            hintText: 'Enter the text',
+                            hintStyle: TextStyle(
+                                color: Colors.grey.withOpacity(0.5)),
+                            contentPadding:
+                                const EdgeInsets.fromLTRB(0, 2, 0, 0),
+                            isDense: true,
+                            border: InputBorder.none,
+                          ),
                         ),
                       ),
-                    ),
-                    InkWell(
-                      onTap: () {
-                        final tasks = List.of(model.tasks!)..removeAt(i);
-                        final newGroup = model.copyWith(tasks: tasks);
+                      InkWell(
+                        onTap: () {
+                          if (item.text!.isEmpty) {
+                            final tasks = List.of(widget.model.tasks!)
+                              ..removeAt(i);
+                            final newGroup =
+                                widget.model.copyWith(tasks: tasks);
 
-                        onChanged(newGroup);
-                      },
-                      child: Icon(
-                        Icons.close,
-                        color: Colors.red.withOpacity(0.4),
-                        size: 12,
+                            widget.onChanged(newGroup);
+                          } else {
+                            showDeleteMsgDialog(item, i);
+                          }
+                        },
+                        child: Icon(
+                          Icons.close,
+                          color: Colors.red.withOpacity(0.4),
+                          size: 12,
+                        ),
                       ),
-                    ),
-                  ],
-                ),
-              );
-            }).toList(),
+                    ],
+                  ),
+                );
+              }).toList(),
+            ),
           )
         ],
       ),
     );
+  }
+
+  void showDeleteMsgDialog(TaskModel model, int index) {
+    showDialog(
+        context: context,
+        builder: (ctx) {
+          return AlertDialog(
+            title: const Text('DELETE?'),
+            actions: [
+              TextButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: const Text('Cancel')),
+              TextButton(
+                  onPressed: () {
+                    final tasks = List.of(widget.model.tasks!)..removeAt(index);
+                    final newGroup = widget.model.copyWith(tasks: tasks);
+
+                    widget.onChanged(newGroup);
+
+                    Navigator.pop(context);
+                  },
+                  child: const Text('OK')),
+            ],
+          );
+        });
+  }
+
+  void copyToCliboard() {
+    String res = '';
+
+    res = res + widget.model.text! + '\n';
+    if (widget.model.tasks!.isNotEmpty) {
+      final listTasks = widget.model.tasks!
+          .where((element) => element.isVisible == true)
+          .toList();
+      if (listTasks.isNotEmpty) {
+        for (var task in listTasks) {
+          res = res + (task.isDone! ? '✓ ' : '☐ ') + task.text! + '\n';
+        }
+      }
+    }
+    log(res);
+    if (res.isNotEmpty) {
+      Clipboard.setData(ClipboardData(text: res));
+      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+        content: Text('All tasks copied to buffer'),
+        duration: Duration(milliseconds: 500),
+      ));
+    }
   }
 }
 
